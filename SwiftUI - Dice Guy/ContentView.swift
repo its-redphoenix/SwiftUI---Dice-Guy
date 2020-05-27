74b1bf484683a07fcb162f4d85c375332fa6b597
Mayur <mayur@vulpe.in>
2020-05-27 16:55:19 +0530

    Dice Guy UI complete

--- SwiftUI - Dice Guy/ContentView.swift
+++ SwiftUI - Dice Guy/ContentView.swift
@@ -10,12 +10,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceV(n: 2)
+                    DiceV(n: 1)
+                }
+                 .padding(.horizontal)
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Keep Rollin'")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                        .background(Color.red)
+                        .cornerRadius(10)
+                    
+                    
+                    
+                    
+                }
+               
+            }
+        }
     }
 }
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceV: View {
+    
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+        .padding()
+            .aspectRatio(1, contentMode: .fit)
     }
 }
